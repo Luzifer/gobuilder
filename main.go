67a1dcc3acd0526ff01968ee0daac40a32e333db
Knut Ahlers <knut@ahlers.me>
2014-11-07 16:06:11 +0100

    Added download generator

--- main.go
+++ main.go
@@ -83,6 +83,27 @@ func main() {
 		// TODO: Handle BitBucket Hooks
 	})
 
+	m.Get("/get/(?P<file>.*)$", func(params martini.Params, res http.ResponseWriter, r *http.Request) {
+		s3auth, err := aws.EnvAuth()
+		if err != nil {
+			log.Error("AWS Authentication Error", loggly.Message{
+				"error": fmt.Sprintf("%v", err),
+			})
+			template := pongo2.Must(pongo2.FromFile("frontend/newbuild.html"))
+			template.ExecuteWriter(pongo2.Context{
+				"error": "An unknown error occured while getting your build.",
+			}, res)
+			return
+		}
+
+		s3conn := s3.New(s3auth, aws.Regions["eu-west-1"])
+		bucket := s3conn.Bucket("gobuild.luzifer.io")
+
+		t := time.Now()
+		t = t.Add(1 * time.Hour)
+		http.Redirect(res, r, bucket.SignedURL(params["file"], t), http.StatusTemporaryRedirect)
+	})
+
 	m.Get("/(?P<repo>.*)$", func(params martini.Params, res http.ResponseWriter, r *http.Request) {
 		branch := r.FormValue("branch")
 		if branch == "" {

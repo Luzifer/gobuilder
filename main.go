5d6359561a78e18a0d169d001837f3fd90b7f7bd
Knut Ahlers <knut@ahlers.me>
2014-11-07 15:52:21 +0100

    Implemented repository view

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 	"os"
 	"time"
 
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
+
+	"github.com/Luzifer/gobuilder/builddb"
 	"github.com/flosch/pongo2"
 	"github.com/go-martini/martini"
 	"github.com/kr/beanstalk"
 	"github.com/segmentio/go-loggly"
+
+	_ "github.com/flosch/pongo2-addons"
 )
 
 var log *loggly.Client
@@ -78,8 +84,60 @@ func main() {
 	})
 
 	m.Get("/(?P<repo>.*)$", func(params martini.Params, res http.ResponseWriter, r *http.Request) {
-		// TODO: Handle repo view
-		res.Write([]byte(fmt.Sprintf("blarg: %s", params["repo"])))
+		branch := r.FormValue("branch")
+		if branch == "" {
+			branch = "master"
+		}
+		buildDBFile := fmt.Sprintf("%s/build.db", params["repo"])
+
+		s3auth, err := aws.EnvAuth()
+		if err != nil {
+			log.Error("AWS Authentication Error", loggly.Message{
+				"error": fmt.Sprintf("%v", err),
+			})
+			template := pongo2.Must(pongo2.FromFile("frontend/newbuild.html"))
+			template.ExecuteWriter(pongo2.Context{
+				"error": "An unknown error occured while getting your build.",
+			}, res)
+			return
+		}
+
+		s3conn := s3.New(s3auth, aws.Regions["eu-west-1"])
+		bucket := s3conn.Bucket("gobuild.luzifer.io")
+
+		file, err := bucket.Get(buildDBFile)
+		if err != nil {
+			log.Error("AWS S3 Get Error", loggly.Message{
+				"error": fmt.Sprintf("%v", err),
+			})
+			template := pongo2.Must(pongo2.FromFile("frontend/newbuild.html"))
+			template.ExecuteWriter(pongo2.Context{
+				"error": "Your build is not yet known to us...",
+				"value": params["repo"],
+			}, res)
+			return
+		}
+
+		var buildDB builddb.BuildDB
+		err = json.Unmarshal(file, &buildDB)
+		if err != nil {
+			log.Error("AWS DB Unmarshal Error", loggly.Message{
+				"error": fmt.Sprintf("%v", err),
+			})
+			template := pongo2.Must(pongo2.FromFile("frontend/newbuild.html"))
+			template.ExecuteWriter(pongo2.Context{
+				"error": "An unknown error occured while getting your build.",
+			}, res)
+			return
+		}
+
+		template := pongo2.Must(pongo2.FromFile("frontend/repository.html"))
+		template.ExecuteWriter(pongo2.Context{
+			"branch":   branch,
+			"builddb":  buildDB,
+			"repo":     params["repo"],
+			"mybranch": buildDB[branch],
+		}, res)
 	})
 
 	m.Run()

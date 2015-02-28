27d41f9c5c051d8a59af022d2e3da6e4df242ec6
Knut Ahlers <knut@ahlers.me>
2015-02-28 14:04:49 +0100

    Added buildlog display

--- main.go
+++ main.go
@@ -139,6 +139,35 @@ func main() {
 		http.Redirect(res, r, bucket.SignedURL(params["file"], t), http.StatusTemporaryRedirect)
 	})
 
+	m.Get("/(?P<repo>.*)/build.log$", func(params martini.Params, res http.ResponseWriter, r *http.Request) {
+		s3auth, err := aws.EnvAuth()
+		if err != nil {
+			log.Error("AWS Authentication Error", loggly.Message{
+				"error": fmt.Sprintf("%v", err),
+			})
+			template := pongo2.Must(pongo2.FromFile("frontend/newbuild.html"))
+			template.ExecuteWriter(pongo2.Context{
+				"error": "An unknown error occured while getting your build.",
+			}, res)
+			return
+		}
+
+		s3conn := s3.New(s3auth, aws.Regions["eu-west-1"])
+		bucket := s3conn.Bucket("gobuild.luzifer.io")
+
+		file, err := bucket.Get(fmt.Sprintf("%s/build.log", params["repo"]))
+		if err != nil {
+			file = []byte("No build log was found for this build.")
+		}
+
+		template := pongo2.Must(pongo2.FromFile("frontend/buildlog.html"))
+		template.ExecuteWriter(pongo2.Context{
+			"repo": params["repo"],
+			"log":  logHighlight(file),
+		}, res)
+
+	})
+
 	m.Get("/(?P<repo>.*)$", func(params martini.Params, res http.ResponseWriter, r *http.Request) {
 		branch := r.FormValue("branch")
 		if branch == "" {
@@ -227,4 +256,25 @@ func sendToQueue(repository string) error {
 	}
 
 	return nil
+}
+
+type logline struct {
+	Line         string
+	BuildComment bool
+}
+
+func logHighlight(log []byte) []logline {
+	lines := strings.Split(string(log), "\n")
+	highlightedLines := []logline{}
+	for _, line := range lines {
+		tmp := logline{
+			Line:         line,
+			BuildComment: false,
+		}
+		if strings.HasPrefix(line, "[") {
+			tmp.BuildComment = true
+		}
+		highlightedLines = append(highlightedLines, tmp)
+	}
+	return highlightedLines
 }

0aa0e7ab7faa756eafc663081e170b6cc9e7d255
Knut Ahlers <knut@ahlers.me>
2015-03-28 14:11:32 +0100

    Added platform icons to file view
    fixes #6

--- filters/filters.go
+++ filters/filters.go
@@ -8,11 +8,18 @@ import (
 
 func init() {
 	pongo2.RegisterFilter("is_mainarch", checkMainArch)
+	pongo2.RegisterFilter("branchicon", getBranchIcon)
 }
 
 func checkMainArch(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
 	str := in.String()
 	s := strings.Split(str, "-")
+	if len(s) < 2 {
+		return nil, &pongo2.Error{
+			Sender:   "filter:branchicon",
+			ErrorMsg: "Field did not contain a valid GoBuilder filename",
+		}
+	}
 	s = strings.Split(s[len(s)-2], "_")
 
 	for _, v := range []string{"linux", "darwin", "windows"} {
@@ -21,4 +28,31 @@ func checkMainArch(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo
 		}
 	}
 	return pongo2.AsValue(false), nil
+}
+
+func getBranchIcon(in *pongo2.Value, param *pongo2.Value) (*pongo2.Value, *pongo2.Error) {
+	str := in.String()
+	s := strings.Split(str, "-")
+	if len(s) < 2 {
+		return nil, &pongo2.Error{
+			Sender:   "filter:branchicon",
+			ErrorMsg: "Field did not contain a valid GoBuilder filename",
+		}
+	}
+	s = strings.Split(s[len(s)-2], "_")
+
+	// Map the architectures used by golang to fontawesome icon names
+	switch s[len(s)-1] {
+	case "darwin":
+		return pongo2.AsValue("apple"), nil
+	case "linux":
+		return pongo2.AsValue("linux"), nil
+	case "windows":
+		return pongo2.AsValue("windows"), nil
+	case "android":
+		return pongo2.AsValue("android"), nil
+	}
+
+	// Not all archs have icons, use a generic file icon
+	return pongo2.AsValue("file"), nil
 }

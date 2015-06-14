8f8ff2a122858c50f02afbcd1ae1a5e9bc42dd53
Knut Ahlers <knut@ahlers.me>
2015-06-14 20:52:19 +0200

    Full refactoring of build-starter
    * Improved code quality
    * Removed panic exits except in initilization
    * Added concurrent builds
    
    fixes #33

---
+++ cmd/starter/background.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/fsouza/go-dockerclient"
+)
+
+func announceActiveWorker() {
+	timestamp := float64(time.Now().Unix())
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Unable to determine hostname")
+	}
+
+	redisClient.ZAdd("active-workers", map[string]float64{
+		hostname: timestamp,
+	})
+
+	// Remove old clients to ensure the redis doesn't get filled with old data
+	redisClient.ZRemRangeByScore("active-workers", "-inf", strconv.Itoa(int(time.Now().Unix()-3600)))
+}
+
+func pullLatestImage() error {
+	auth := docker.AuthConfiguration{}
+	authConfig, err := docker.NewAuthConfigurationsFromDockerCfg()
+	if err != nil {
+		return err
+	}
+
+	reginfo := strings.SplitN(os.Getenv("BUILD_IMAGE"), "/", 2)
+	if len(reginfo) == 2 {
+		for s, a := range authConfig.Configs {
+			if strings.Contains(s, fmt.Sprintf("://%s/", reginfo[0])) {
+				auth = a
+			}
+		}
+	}
+
+	err = dockerClient.PullImage(docker.PullImageOptions{
+		Repository: os.Getenv("BUILD_IMAGE"),
+		Tag:        "latest",
+	}, auth)
+
+	return err
+}
